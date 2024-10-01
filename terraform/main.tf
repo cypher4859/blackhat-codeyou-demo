0c4dad2b9fbd85a7ef55a853c35b0ce54b041f85
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2024-10-01 10:41:37 -0400

    Setup the entrypoint of terraform to use remote s3 state backend Added profile for auth'ing Added the module for compute infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,36 @@
 terraform {
     required_providers {
         aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.0"
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
         }
+    }
+    backend "s3" {
+        bucket = "blackhat-codeyou-demo-terraform-bucket"
+        key    = "terraform/state/blackhat-demo/code-you/terraform.tfstate"
+        region = "us-east-2"
+        encrypt = true
+        
     }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2"
+    profile = "blackhat-user"
 }
 
 # Create network infrastructure
 module "blackhat_network_infra" {
     source = "./modules/network-infra"
-    vpc_id = ""
+    vpc_id = "vpc-3771c65c" # Grabbed from the console
 }
-# Create the compute infrastructure (ALB)
-# Create the ECS Infra
+
+module "blackhat_compute_infra" {
+    source = "./modules/compute-infra"
+    security_groups = compact(module.blackhat_network_infra.security_groups)
+    vpc_id = module.blackhat_network_infra.vpc_id
+    subnets = compact(module.blackhat_network_infra.subnets)
+    launch_template_key_name = "blackhat_codeyou_demo_keypair"
+    kali_subnet = module.blackhat_network_infra.public_subnet
+}
